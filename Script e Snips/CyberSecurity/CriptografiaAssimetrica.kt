961186c48b4adcdd60d1ba602abfaa02d53f1105
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-30 18:35:22 -0300

    Update CriptografiaAssimetrica.kt

--- Script e Snips/CyberSecurity/CriptografiaAssimetrica.kt
+++ Script e Snips/CyberSecurity/CriptografiaAssimetrica.kt
@@ -1,1 +1,3 @@
 // Script apenas para entender a logica. O objetivo é o uso da criptografia simetrica e assimetrica, onde envio uma chave AES, criptografando com a chave publica, e descriptografando com a chave privada, ambas chaves RSA. Após isso, encripto uma mensagem usando a chave AES, e descriptogradando com a mesma.
+
+import java.security.KeyPair
